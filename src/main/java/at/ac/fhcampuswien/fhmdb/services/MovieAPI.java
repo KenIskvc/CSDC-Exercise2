2cf477ff064141d57e2bdd87423ec41f43dc1dcc
KenIskvc <kenan.sosic@gmx.net>
2025-03-10 18:50:31 +0100

    Added Jackson for JSON Parsing, Implemented Tests for MovieAPI. Extended MovieAPI Methods with params.

--- src/main/java/at/ac/fhcampuswien/fhmdb/services/MovieAPI.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/services/MovieAPI.java
@@ -1,9 +1,6 @@
 package at.ac.fhcampuswien.fhmdb.services;
 
-import okhttp3.Call;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
+import okhttp3.*;
 
 import java.io.IOException;
 
@@ -17,9 +14,44 @@ public class MovieAPI {
     }
 
     //not tested yet
-    public String GetMovies() throws IOException {
+    public String GetAllMovies() throws IOException {
         Request request = new Request.Builder()
                 .url(BASE_URL + "/movies")
+                .header("User-Agent", "UserAgentGroup/1.3")
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            return response.body().string();
+        } catch (IOException e) {
+            System.out.println("Fetching movies failed");
+            System.out.println("GetMovies()-Error: " + e.getMessage());
+            return null;
+        }
+    }
+    public String GetMoviesFromQuery(String query) throws IOException {
+        HttpUrl.Builder urlBuilder
+                = HttpUrl.parse(BASE_URL + "/movies").newBuilder();
+        urlBuilder.addQueryParameter("query", query);
+        String url = urlBuilder.build().toString();
+
+        Request request = new Request.Builder()
+                .url(url)
+                .header("User-Agent", "UserAgentGroup/1.3")
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            return response.body().string();
+        } catch (IOException e) {
+            System.out.println("Fetching movies failed");
+            System.out.println("GetMovies()-Error: " + e.getMessage());
+            return null;
+        }
+    }
+
+    public String GetFilteredMovies(String query, String genre, int releaseYear, double ratingForm) throws IOException {
+        Request request = new Request.Builder()
+                .url(BASE_URL + "/movies")
+                .header("User-Agent", "UserAgentGroup/1.3")
                 .build();
 
         try (Response response = client.newCall(request).execute()) {
@@ -32,6 +64,9 @@ public class MovieAPI {
     }
 
     public String GetMovie() throws IOException {
-        throw new IOException();
+        Request request = new Request.Builder()
+                .url(BASE_URL + "/movies")
+                .build();
+        return null;
     }
 }
