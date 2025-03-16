150ee4e0ec28620497812bceca72c89bf7eb70a2
harun sefer <harunseferoffice@gmail.com>
2025-03-16 13:54:19 +0100

    Movie utils

--- src/main/java/at/ac/fhcampuswien/fhmdb/models/Movie.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/models/Movie.java
@@ -8,11 +8,17 @@ public class Movie {
     private final String title;
     private final String description;
     private final List<Genre> genres;
+    private final List<String> mainCast; // Liste der Hauptdarsteller
+    private final String director; // Regisseur
+    private final int releaseYear; // Erscheinungsjahr
 
-    public Movie(String title, String description, List<Genre> genres) {
+    public Movie(String title, String description, List<Genre> genres, List<String> mainCast, String director, int releaseYear) {
         this.title = title;
         this.description = description;
         this.genres = genres;
+        this.mainCast = mainCast;
+        this.director = director;
+        this.releaseYear = releaseYear;
     }
 
     @Override
@@ -41,29 +47,57 @@ public class Movie {
         return genres;
     }
 
+    public List<String> getMainCast() {
+        return mainCast;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public int getReleaseYear() {
+        return releaseYear;
+    }
+
     public static List<Movie> initializeMovies(){
         List<Movie> movies = new ArrayList<>();
         movies.add(new Movie(
                 "Life Is Beautiful",
-                "When an open-minded Jewish librarian and his son become victims of the Holocaust, he uses a perfect mixture of will, humor, and imagination to protect his son from the dangers around their camp." ,
-                Arrays.asList(Genre.DRAMA, Genre.ROMANCE)));
+                "When an open-minded Jewish librarian and his son become victims of the Holocaust, he uses a perfect mixture of will, humor, and imagination to protect his son from the dangers around their camp.",
+                Arrays.asList(Genre.DRAMA, Genre.ROMANCE),
+                Arrays.asList("Roberto Benigni", "Nicoletta Braschi"),
+                "Roberto Benigni",
+                1997));
         movies.add(new Movie(
                 "The Usual Suspects",
                 "A sole survivor tells of the twisty events leading up to a horrific gun battle on a boat, which begin when five criminals meet at a seemingly random police lineup.",
-                Arrays.asList(Genre.CRIME, Genre.DRAMA, Genre.MYSTERY)));
+                Arrays.asList(Genre.CRIME, Genre.DRAMA, Genre.MYSTERY),
+                Arrays.asList("Kevin Spacey", "Gabriel Byrne"),
+                "Bryan Singer",
+                1995));
         movies.add(new Movie(
                 "Puss in Boots",
                 "An outlaw cat, his childhood egg-friend, and a seductive thief kitty set out in search for the eggs of the fabled Golden Goose to clear his name, restore his lost honor, and regain the trust of his mother and town.",
-                Arrays.asList(Genre.COMEDY, Genre.FAMILY, Genre.ANIMATION)));
+                Arrays.asList(Genre.COMEDY, Genre.FAMILY, Genre.ANIMATION),
+                Arrays.asList("Antonio Banderas", "Salma Hayek"),
+                "Chris Miller",
+                2011));
         movies.add(new Movie(
                 "Avatar",
                 "A paraplegic Marine dispatched to the moon Pandora on a unique mission becomes torn between following his orders and protecting the world he feels is his home.",
-                Arrays.asList(Genre.ANIMATION, Genre.DRAMA, Genre.ACTION)));
+                Arrays.asList(Genre.ANIMATION, Genre.DRAMA, Genre.ACTION),
+                Arrays.asList("Sam Worthington", "Zoe Saldana"),
+                "James Cameron",
+                2009));
         movies.add(new Movie(
                 "The Wolf of Wall Street",
                 "Based on the true story of Jordan Belfort, from his rise to a wealthy stock-broker living the high life to his fall involving crime, corruption and the federal government.",
-                Arrays.asList(Genre.DRAMA, Genre.ROMANCE, Genre.BIOGRAPHY)));
+                Arrays.asList(Genre.DRAMA, Genre.ROMANCE, Genre.BIOGRAPHY),
+                Arrays.asList("Leonardo DiCaprio", "Jonah Hill"),
+                "Martin Scorsese",
+                2013));
 
         return movies;
     }
 }
+
